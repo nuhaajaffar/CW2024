2feb9576a4a9aa0a7f4f434441adc2b44c5eaa3b
nuhaajaffar <164647310+nuhaajaffar@users.noreply.github.com>
2024-12-13 13:53:51 +0800

    Made Level 3 harder with more enemy planes approaching

--- src/main/java/com/example/demo/levels/LevelThree.java
+++ src/main/java/com/example/demo/levels/LevelThree.java
@@ -9,7 +9,7 @@ import javafx.scene.text.Text;
 
 public class LevelThree extends LevelParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
-    private static final int TOTAL_ENEMIES = 5;
+    private static final int TOTAL_ENEMIES = 8;
     private static final int KILLS_TO_ADVANCE = 30;
     private static final double ENEMY_SPAWN_PROBABILITY = .20;
     private static final int PLAYER_INITIAL_HEALTH = 5;
